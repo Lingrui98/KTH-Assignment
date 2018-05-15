c1e2c40fae3176bfecdd1b6ffa768b7ebd7e4d48
Lingrui98 <goulingrui15@gmail.com>
2018-05-15 17:24:04 +0200

    Add files via upload

--- As3.py
+++ As3.py
@@ -6,23 +6,23 @@ import matplotlib.pyplot as plt
 
 BN = True
 
-batch_size = 1
+batch_size = 100
 eta = 0.01
-lamda = 0.00
-epoch = 10
+lamda = 0.0001
+epoch = 20
 
 # initialization = 'He'
 initialization = 'Xavier'
 # initialization = 0
 
-n_layers = 2
+n_layers = 3
 
 out_size = 10
 in_size = 3072
-m_nodes = [in_size,50,out_size]
-rho = 0
+m_nodes = [in_size,50,30,out_size]
+rho = 0.9
 
-CHECK = True
+CHECK = False
 
 SVM = 0
 CrossEntropy = 1
@@ -59,7 +59,7 @@ def plot_cost(tr,val):
 
 class Classifier(object):
     """docstring for Classifier"""
-    def __init__(self, n_layers, m_nodes, init, rho, BN, eta=0.1, lamda=0, epoch=100, batch_size=batch_size, reader=None):
+    def __init__(self, n_layers, m_nodes, init, rho, BN, eta=0.1, lamda=0, epoch=20, batch_size=batch_size, reader=None):
         super(Classifier, self).__init__()
         self.out_size = m_nodes[-1]
         self.in_size = m_nodes[0]
@@ -77,7 +77,7 @@ class Classifier(object):
 
         self.set = False # indicates whether mu_av and var_av are set
 
-
+        # len(W) equals to the number of layers
         W = []
         for i in xrange(n_layers):
             sigma = 0.0
@@ -90,7 +90,6 @@ class Classifier(object):
                     sigma = 0.001
             print "sigma = %f in layer %d" % (sigma, i+1)
             W.append(np.random.normal(0,sigma, (m_nodes[i+1],m_nodes[i])))
-        # print "len W = ", len(W)
         self.W = W
 
         b = []
@@ -101,7 +100,7 @@ class Classifier(object):
 
         self.batch_size = batch_size
         # self.RW = np.sum(np.square(self.W)) * self.lambda_
-        self.reader = reader
+        # self.reader = reader
 
     def softmax(self,s):
         s = np.exp(s)
@@ -118,14 +117,15 @@ class Classifier(object):
         return s_hat
 
 # Forward pass
-    def EvaluateClassifier(self, X, W, b, update=False):
+    def EvaluateClassifier(self, X, W, b, update=False, test=False, mu_a=None, var_a=None):
         n = X.shape[1]
         P = np.zeros((self.out_size,n))
         P = np.reshape(P,(self.out_size,n))
 
-        s_rec = [[np.reshape(X[:,i],(self.in_size,1))] for i in xrange(n)]
-        s_hat = [[] for i in xrange(n)]
+        s_rec = [[np.reshape(X[:,i],(self.in_size,1))] for i in xrange(n)] # record of un-normalized scores
+        s_hat = [[] for i in xrange(n)] # normalized scores
         x = [[s_rec[i][0]] for i in xrange(n)]
+
         mu = []
         var = []
 
@@ -145,7 +145,13 @@ class Classifier(object):
                 var.append(v / float(n))
 
                 for j in xrange(n):
-                    s_hat[j].append(self.BatchNormalize(s_rec[j][i+1],mu[i],var[i],1e-5))
+                    if test == True:
+                        if mu_a==None or var_a==None:
+                            s_hat[j].append(self.BatchNormalize(s_rec[j][i+1],self.mu_av[i],self.var_av[i],1e-5))
+                        else:
+                            s_hat[j].append(self.BatchNormalize(s_rec[j][i+1],mu_a[i],var_a[i],1e-5))
+                    else:
+                        s_hat[j].append(self.BatchNormalize(s_rec[j][i+1],mu[i],var[i],1e-5))
                     x[j].append(np.abs((np.abs(s_hat[j][i])+s_hat[j][i]) / 2))
             else:
                 for j in xrange(n):
@@ -186,9 +192,12 @@ class Classifier(object):
         J += lambda_ * R
         return J
 
-    def ComputeAccuracy(self, X, Y, W, b):
+    def ComputeAccuracy(self, X, Y, W, b, test=False, mu=None, var=None):
         n = X.shape[1]
-        P = self.EvaluateClassifier(X, W, b)
+        if mu==None or var==None:
+            P = self.EvaluateClassifier(X, W, b,test=test)
+        else:
+            P = self.EvaluateClassifier(X, W, b,test=test,mu_a=mu,var_a=var)
         labels = np.array([np.argmax(Y[:,i]) for i in xrange(n)])
         out = np.array([np.argmax(P[:,i]) for i in xrange(n)])
         num = 0.0
@@ -284,7 +293,7 @@ class Classifier(object):
         print "Using %s initialization\n" % (initialization)
         print "The structure is ", self.m_nodes
         print "initial eta = %3.2f" % (self.learning_rate)
-        print "lambda = %3.2f" % (self.lambda_)
+        print "lambda = %5.4f" % (self.lambda_)
         print "max_epoch = %d" % (self.max_epoch)
         print "rho = %3.2f" % (self.rho)
         print "alpha = %3.2f\n" % (self.alpha)
@@ -292,57 +301,40 @@ class Classifier(object):
 
     def Train(self, loss_type = CrossEntropy):
         ep = 0
-
-        X_mean = np.reshape(np.zeros(3072), (3072,1))
-
-        X_train, Y_train = self.reader.next_train_data(10000)
-        Y_train = np.array(Y_train).T
-        X_train = np.reshape(np.array(X_train),(10000,3072)).T / 256.0 #normaliztion
-        for i in xrange(3072):
-            X_mean[i] = np.mean(X_train[i])
-        X_train = X_train - X_mean
-
-        X_val, Y_val = self.reader.next_train_data(10000)
-        Y_val = np.array(Y_val).T
-        X_val = np.reshape(np.array(X_val),(10000,3072)).T / 256.0 #normaliztion
-        X_val = X_val - X_mean
-
-        X_test, Y_test = self.reader.next_test_data()
-        Y_test = np.array(Y_test).T
-        X_test = np.reshape(np.array(X_test) , (10000,3072)).T / 256.0
-        X_test = X_test - X_mean
+        self.print_pram()
 
         train = []
         validate = []
         test = []
         cost = []
 
-        print "Data loaded, training starts..."
-        self.print_pram()
+        best_W = [[] for i in xrange(self.n_layers)]
+        best_b = [[] for i in xrange(self.n_layers)]
+        best_mu = [[] for i in xrange(self.n_layers)]
+        best_var = [[] for i in xrange(self.n_layers)]
+        max_val_acc = 0.0
+
+        eta = self.learning_rate
 
         for ep in xrange(self.max_epoch):
-            #print "Epoch %d, " % (ep+1)
             batch_index = 0
-            v = []
 
+            v = [] # the momentum
+
+            # if (ep+1) % 1 == 0: # decline of learning rate
+            #     eta *= 1.0
+
+            # Mini-batch training in one epoch
             for batch_index in xrange(10000//self.batch_size):
                 X = np.reshape(X_train[:,batch_index*self.batch_size:(batch_index+1)*self.batch_size],(3072,self.batch_size))
-                #print X.shape
                 Y = Y_train[:,batch_index*self.batch_size:(batch_index+1)*self.batch_size]
-                #print Y.shape
-                # P = self.EvaluateClassifier(X, self.W, self.b, update=True)
 
                 grad_W, grad_b = self.ComputeGradients(X, Y, self.W, self.b, self.lambda_)
-
 
                 # The evaluation part for the gradients
                 if (CHECK==True):
                     print "Checking gradients..."
-                    # print self.b
-                    # gWt, gbt = self.ComputeGradsNumSlow(X,Y,self.W,self.b,self.lambda_,1e-5)
-
                     gWt, gbt = self.ComputeGradsNum(X,Y,self.W,self.b,self.lambda_,1e-5)
-                    # gbt = self.ComputeGradsNum(X,Y,self.W,self.b,self.lambda_,1e-5)
 
                     D_gW = []
                     D_gb = []
@@ -350,55 +342,63 @@ class Classifier(object):
                         D_gW.append(grad_W[i] - gWt[i])
                         D_gb.append(grad_b[i] - gbt[i])
 
-                    # print grad_b, "\n", gbt, "\n", "D=\n", D_gb
                     eps = 1e-5
-                    # print np.mean(grad_W[0]), np.mean(gWt[0])
                     E_gW = []
                     E_gb = []
                     for i in xrange(self.n_layers):
                         E_gW.append(np.mean(np.abs(D_gW[i])) / max(eps, np.mean(np.abs(grad_W[i])+np.abs(gWt[i]))))
                         E_gb.append(np.mean(np.abs(D_gb[i])) / max(eps, np.mean(np.abs(grad_b[i])+np.abs(gbt[i]))))
-
-                    # print E_gb
                     print E_gW, E_gb
 
-                # J = self.ComputeCost(X,Y,self.W,self.b,self.lambda_)
-                # print "Cost = %f in batch %d" % (J,batch_index)
+                # Updating the gradient
                 if batch_index==0:
                     for i in xrange(self.n_layers):
-                        self.W[i] += -self.learning_rate * grad_W[i]
-                        self.b[i] += -self.learning_rate * grad_b[i]
+                        self.W[i] += -eta * grad_W[i]
+                        self.b[i] += -eta * grad_b[i]
                 else:
                     for i in xrange(self.n_layers):
-                        self.W[i] += -self.learning_rate * grad_W[i] - self.rho * v[2*i]
-                        self.b[i] += -self.learning_rate * grad_b[i] - self.rho * v[2*i+1]
+                        self.W[i] += -eta * grad_W[i] - self.rho * v[2*i]
+                        self.b[i] += -eta * grad_b[i] - self.rho * v[2*i+1]
                 v = []
                 for i in xrange(self.n_layers):
-                    v.append(self.learning_rate * grad_W[i])
-                    v.append(self.learning_rate * grad_b[i])
-                # print v
-                #print grad[0].shape
+                    v.append(eta* grad_W[i])
+                    v.append(eta * grad_b[i])
 
-                # print "self.b = \n", self.b
-                # print self.W,self.b
-
+            # Compute the cost
             J_tr = self.ComputeCost(X_train,Y_train,self.W,self.b,self.lambda_)
             J_val = self.ComputeCost(X_val,Y_val,self.W,self.b,self.lambda_)
             cost.append([J_tr,J_val])
 
+            # Compute the accuracy
             tr_acc = self.ComputeAccuracy(X_train,Y_train,self.W,self.b)
-            v_acc = self.ComputeAccuracy(X_val,Y_val,self.W,self.b)
-            t_acc = self.ComputeAccuracy(X_test,Y_test,self.W,self.b)
-
+            v_acc = self.ComputeAccuracy(X_val,Y_val,self.W,self.b,test=True)
+            t_acc = self.ComputeAccuracy(X_test,Y_test,self.W,self.b,test=True)
             train.append(tr_acc*100)
             validate.append(v_acc*100)
             test.append(t_acc*100)
 
-            print("Epoch %2d, train acc= %4.2f%%, validate acc = %4.2f%%, test acc = %4.2f%%" % (ep+1, tr_acc*100, v_acc*100, t_acc*100))
-        t_acc = test[-1]
-        print "Final accuracy on test set = %4.2f%%" % (t_acc)
+            # Keep a record of the best result
+            if v_acc > max_val_acc:
+                max_val_acc = v_acc
+                for i in xrange(self.n_layers):
+                    best_W[i] = self.W[i].copy()
+                    best_b[i] = self.b[i].copy()
+                    best_mu[i] = self.mu_av[i].copy()
+                    best_var[i] = self.var_av[i].copy()
 
-        return train, validate, test, cost
+
+            print("Epoch %2d with eta = %4.3f, train acc= %4.2f%%, validate acc = %4.2f%%, test acc = %4.2f%%" % (ep+1, eta, tr_acc*100, v_acc*100, t_acc*100))
+
+        t_acc = test[-1]
+        best_result = self.ComputeAccuracy(X_test,Y_test,best_W,best_b,test=True,mu=best_mu,var=best_var) * 100
+        print "Final accuracy on test set = %4.2f%%, theoretical best result during training is %4.2f%%" % (t_acc,best_result)
+
+        if self.BN == True:
+            best = [best_result, best_W, best_b, best_mu, best_var]
+        else:
+            best = [best_result, best_W, best_b]
+
+        return train, validate, test, cost, best
 
 
 class Cifar10DataReader():
@@ -479,9 +479,52 @@ if __name__=="__main__":
     # Initializing the reader
     dr=Cifar10DataReader(cifar_folder="../cifar-10-batches-py/")
     # Initializing classifier
-    c = Classifier(n_layers,m_nodes,initialization,rho,BN,eta,lamda,epoch,reader=dr)
+    X_mean = np.reshape(np.zeros(3072), (3072,1))
 
-    tr, val, t, cost = c.Train()
+    print "Loading training data..."
+    X_train, Y_train = dr.next_train_data(10000)
+    Y_train = np.array(Y_train).T
+    X_train = np.reshape(np.array(X_train),(10000,3072)).T / 256.0 #normaliztion
+    for i in xrange(3072):
+        X_mean[i] = np.mean(X_train[i])
+    X_train = X_train - X_mean
 
-    plot_accuracy(tr,val,t)
-    plot_cost(np.array(cost)[:,0],np.array(cost)[:,1])
+    print "Loading validation data..."
+    X_val, Y_val = dr.next_train_data(10000)
+    Y_val = np.array(Y_val).T
+    X_val = np.reshape(np.array(X_val),(10000,3072)).T / 256.0 #normaliztion
+    X_val = X_val - X_mean
+
+    print "Loading test data..."
+    X_test, Y_test = dr.next_test_data()
+    Y_test = np.array(Y_test).T
+    X_test = np.reshape(np.array(X_test) , (10000,3072)).T / 256.0
+    X_test = X_test - X_mean
+
+    print "Data loaded..."
+
+    # c = Classifier(n_layers,m_nodes,initialization,rho,BN,eta,lamda,epoch,reader=dr)
+
+    best_arr = [[[0,0],0] for i in xrange(3)] # [[eta,lambda], best_accuracy]
+    print best_arr
+
+    for i in xrange(5):
+        lr = 0.1 / float((i+1)*(i+1)) 
+        for j in xrange(5):
+            ld = j*0.002 # [0,0.01]
+            a = Classifier(n_layers,m_nodes,initialization,rho,BN,lr,ld,epoch,reader=dr)
+            tr, val, t, cost, best = a.Train()
+            if best[0] > best_arr[0][1]:
+                # print best_arr
+                del best_arr[-1]
+                best_arr = [[[lr,ld], best[0]]] + best_arr
+                # print best_arr
+
+    print "best accuracy achieved with eta = %f, lambda = %f, best_accuracy = %4.2f%%" % (best_arr[0][0][0], best_arr[0][0][1], best_arr[0][1])
+    print "best_arr is" , best_arr
+
+
+    # tr, val, t, cost, best = c.Train()
+
+    # plot_accuracy(tr,val,t)
+    # plot_cost(np.array(cost)[:,0],np.array(cost)[:,1])
