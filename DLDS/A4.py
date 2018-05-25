b8422a02e8e65241f19556c82234e3e0ccc01e7e
Lingrui98 <goulingrui15@gmail.com>
2018-05-25 19:02:41 +0200

    Complete version, training data is Harry Potter IV

--- DLDS/A4.py
+++ DLDS/A4.py
@@ -1,5 +1,7 @@
 import numpy as np
 import string
+import copy
+import matplotlib.pyplot as plt
 
 DEBUG = 0
 # d: number of unique characters, length of input vectors
@@ -12,11 +14,24 @@ hid_m = 100
 seq_length = 25
 
 MAX_EPOCH = 1
+CHECK = False
+
+def plot_loss(l):
+    ite = []
+    for i in range(len(l)):
+        ite.append(i+1)
+    fig = plt.plot(ite,l,'b')
+    plt.title('The training process')
+    plt.xlabel('Iterations')
+    plt.ylabel('Smooth loss')
+    plt.legend(loc='best')
+    plt.savefig('loss.png')
+    plt.show()
 
 def read_txt(name):
     with open(name, 'r') as file:
-        text = file.read()
-    return text
+        texts = file.read()
+    return texts
 
 def get_unique_chars(str):
     char_list = list(set(str))
@@ -57,7 +72,7 @@ class RNN(object):
     def __init__(self, data, in_size, out_size, hid_size, eta, seq_length, dict):
         super(RNN, self).__init__()
         self.data = data
-        print (len(data))
+        # print (len(data))
         self.in_size = in_size
         self.out_size = out_size
         self.hid_size = hid_size
@@ -69,9 +84,9 @@ class RNN(object):
         self.h0 = np.reshape(np.zeros(hid_size), (hid_size,1))
         self.hprev = self.h0.copy()
 
-        self.para = {'W': np.random.normal(0, 1, (hid_size, hid_size)) * 0.1,
-                     'U': np.random.normal(0, 1, (hid_size,  in_size)) * 0.1,
-                     'V': np.random.normal(0, 1, (out_size, hid_size)) * 0.1,
+        self.para = {'W': np.random.normal(0, 1, (hid_size, hid_size)) * 0.01,
+                     'U': np.random.normal(0, 1, (hid_size,  in_size)) * 0.01,
+                     'V': np.random.normal(0, 1, (out_size, hid_size)) * 0.01,
                      'b': np.reshape(np.zeros(hid_size), (hid_size,1)),
                      'c': np.reshape(np.zeros(out_size), (out_size,1))}
         self.grad = {'W': np.reshape(np.zeros(self.hid_size*self.hid_size), (self.hid_size,self.hid_size)),
@@ -80,14 +95,25 @@ class RNN(object):
                      'b': np.reshape(np.zeros(hid_size), (hid_size,1)),
                      'c': np.reshape(np.zeros(out_size), (out_size,1))}
 
-
         # for name in self.fieldnames:
         #     print (self.para[name])
 
         self.char_to_ind = dict[0]
         self.ind_to_char = dict[1]
 
-        self.CHECK_GRADS = True
+        self.CHECK_GRADS = CHECK
+
+        self.ADA = True
+        self.CLIP = False
+
+    def init_para(self):
+        para  = {'W': np.reshape(np.zeros(self.hid_size*self.hid_size), (self.hid_size,self.hid_size)),
+                 'U': np.reshape(np.zeros(self.in_size *self.hid_size), (self.hid_size,self.in_size)),
+                 'V': np.reshape(np.zeros(self.out_size*self.hid_size), (self.out_size,self.hid_size)),
+                 'b': np.reshape(np.zeros(self.hid_size), (self.hid_size,1)),
+                 'c': np.reshape(np.zeros(self.out_size), (self.out_size,1))
+        }
+        return para
 
     def softmax(self,s):
         s = np.exp(s)
@@ -96,6 +122,7 @@ class RNN(object):
         return s
 
     def string_to_matrix(self, str):
+        # print (str)
         l = len(str) # should be 25
         for i in range(l):
             v = ind_to_vec(char_to_ind[str[i]])
@@ -107,7 +134,7 @@ class RNN(object):
         return M
 
 
-    def generate_texts(self, length, h0, x0):
+    def generate_texts(self, length, h0, x0, para):
         n = length
         texts = []
         h = h0
@@ -115,9 +142,9 @@ class RNN(object):
         ind = to_ind(x)
         texts.append(self.ind_to_char[ind])
         for i in range(n):
-            a = np.dot(self.para['W'],h) + np.dot(self.para['U'],x) + self.para['b']
+            a = np.dot(para['W'],h) + np.dot(para['U'],x) + para['b']
             h = np.tanh(a)
-            o = np.dot(self.para['V'],h) + self.para['c']
+            o = np.dot(para['V'],h) + para['c']
             p = self.softmax(o)
 
             s = np.cumsum(p)
@@ -131,13 +158,14 @@ class RNN(object):
         return
         # return texts
 
-    def grad_clip(self):
+    def grad_clip(self, grads):
+        clipped = self.init_para()
         for f in self.fieldnames:
             shape = self.grad[f].shape
             for i in range(shape[0]):
                 for j in range(shape[1]):
-                    self.grads[f][i][j] = max(min(self.grads[f][i][j], 5), -5)
-        return
+                    clipped[f][i][j] = max(min(grads[f][i][j], 5), -5)
+        return clipped
 
     def get_data(self, e):
         X = self.data[e   : e+self.seq_length]
@@ -146,129 +174,169 @@ class RNN(object):
         Y = self.string_to_matrix(Y)
         return X, Y
 
-    def forward_pass(self, para, hprev, X, Y):
+    def forward_pass(self, para, hp, X, Y):
+        # Using input parameter hprev instead of self.prev
+        # Does not change self.hprev
+        assert X.shape[1] == self.seq_length
+        assert Y.shape[1] == self.seq_length
+
+        h_in = hp
+        H = [hp]
         A = []
-        H = [hprev]
         P = []
         L = 0.0
-        for i in range(seq_length):
+        for i in range(self.seq_length):
             x = np.reshape(X[:,i], (self.in_size , 1))
             y = np.reshape(Y[:,i], (self.out_size, 1))
-            a = np.dot(para['W'],hprev) + np.dot(para['U'],x) + para['b']
+            a = np.dot(para['W'],H[i]) + np.dot(para['U'],x) + para['b']
             h = np.tanh(a)
             o = np.dot(para['V'],h) + para['c']
             p = self.softmax(o)
-            A.append(a)
+            # print ('p shape', p.shape)
+            # print ('y_sum', np.sum(y))
             H.append(h)
+            A.append(a)
             P.append(p)
+            # print ('is', np.dot(y.T,p))
             l = -np.log(np.dot(y.T,p))[0][0]
             L += l
-            self.hprev = h
+            # print ('l=', l)
+        # self.hprev = h_in
+        # print ('L=', L)
+        # print ('len H', len(H))
         return L, H, A, P
 
     def compute_gradients(self, X, Y, H, A, P):
-        grads = {'W': np.reshape(np.zeros(self.hid_size*self.hid_size), (self.hid_size,self.hid_size)),
-                 'U': np.reshape(np.zeros(self.in_size *self.hid_size), (self.hid_size,self.in_size)),
-                 'V': np.reshape(np.zeros(self.out_size*self.hid_size), (self.out_size,self.hid_size)),
-                 'b': np.reshape(np.zeros(self.hid_size), (self.hid_size,1)),
-                 'c': np.reshape(np.zeros(self.out_size), (self.out_size,1))
-        }
+        # Does not change self.hprev
+        grads = self.init_para()
 
         G = []
         for i in range(self.seq_length):
             y = np.reshape(Y[:,i], (self.out_size, 1))
             p = P[i]
             g = -(y-p).T
+            # print ('g=',g)
+            # print ('sum g =', np.sum(g))
             G.append(g)
         for i in range(self.seq_length):
             grads['V'] += np.dot(np.reshape(G[i], (self.out_size,1)), np.reshape(H[i+1], (1, self.hid_size)))
-
-        dht = np.dot(np.reshape(G[self.seq_length-1], (1,self.out_size)), self.para['V'])
+            grads['c'] += np.reshape(G[i], (self.out_size,1))
+        assert len(G) == self.seq_length
+        dht = np.dot(np.reshape(G[-1], (1,self.out_size)), self.para['V'])
         # print ((1-np.square(np.tanh(A[self.seq_length-1]))).shape)
-        diagat = np.diag(np.reshape((1-np.square(np.tanh(A[self.seq_length-1]))), (self.hid_size,)))
+        diagat = np.diag(np.reshape((1-np.tanh(A[-1])*np.tanh(A[-1])), (self.hid_size,)))
         # print (diagat.shape)
         dat = np.dot(dht,diagat)
-        grads['W'] += np.dot(dat.T, H[self.seq_length-1].T)
-        grads['U'] += np.dot(dat.T, np.reshape(X[:,self.seq_length-1], (1, self.in_size)))
-        for i in range(self.seq_length-1):
-            t = self.seq_length - 2 -i
+        grads['b'] += np.reshape(dat, (self.hid_size,1))
+        grads['W'] += np.dot(dat.T, H[-2].T)
+        grads['U'] += np.dot(dat.T, np.reshape(X[:,-1], (1, self.in_size)))
+        for t in reversed(range(self.seq_length-1)):
+            # print (t)
             dht = np.dot(G[t],self.para['V']) + np.dot(dat, self.para['W'])
-            diagat = np.diag(np.reshape((1-np.square(np.tanh(A[t]))), (self.hid_size,)))
+            diagat = np.diag(np.reshape((1-np.tanh(A[t])*np.tanh(A[t])), (self.hid_size,)))
             dat = np.dot(dht,diagat)
+            grads['b'] += np.reshape(dat, (self.hid_size,1))
             grads['W'] += np.dot(dat.T, H[t].T)
             grads['U'] += np.dot(dat.T, np.reshape(X[:,t], (1, self.in_size)))
-
+        # print (grads)
         return grads
 
-    def ComputeGradsNum(self, X, Y, h):
-        grads = {'W': np.reshape(np.zeros(self.hid_size*self.hid_size), (self.hid_size,self.hid_size)),
-                 'U': np.reshape(np.zeros(self.in_size *self.hid_size), (self.hid_size,self.in_size)),
-                 'V': np.reshape(np.zeros(self.out_size*self.hid_size), (self.out_size,self.hid_size)),
-                 'b': np.reshape(np.zeros(self.hid_size), (self.hid_size,1)),
-                 'c': np.reshape(np.zeros(self.out_size), (self.out_size,1))
-        }
-        hprev = self.hprev
+    def ComputeGradsNum(self, X, Y, hp, h):
+        # Calls the function forward_pass twice
+        grads = self.init_para()
+        # hprev = self.hprev
+        # para = self.para.copy()
         for f in self.fieldnames:
             print ('Computing numeriacal gradient for %s' % (f))
             for r in range(self.para[f].shape[0]):
                 for c in range(self.para[f].shape[1]):
-                    para_try = self.para.copy()
+                    para_try = copy.deepcopy(self.para)
                     para_try[f][r][c] = self.para[f][r][c] - h
-                    l1, _, __, ___ = self.forward_pass(para_try, hprev, X, Y)
+                    # print ('para_try-[%s][%d][%d] = %f, para = %f' %(f,r,c,para_try[f][r][c],self.para[f][r][c]))
+                    l1, _, __, ___ = self.forward_pass(para_try, hp, X, Y)
+                    para_try = copy.deepcopy(self.para)
                     para_try[f][r][c] = self.para[f][r][c] + h
-                    l2, _, __, ___ = self.forward_pass(para_try, hprev, X, Y)
+                    # print ('para_try+[%s][%d][%d] = %f, para = %f' %(f,r,c,para_try[f][r][c],self.para[f][r][c]))
+                    l2, _, __, ___ = self.forward_pass(para_try, hp, X, Y)
                     grads[f][r][c] = (l2-l1) / (2*h)
+        # print (grads['b'],grads['c'])
+        # for f in self.fieldnames:
+        #     assert (para[f] == self.para[f]).all()
         return grads
 
     def backward_pass(self, X, Y, H, A, P):
-        hprev = self.hprev
         grads = self.compute_gradients(X, Y, H, A, P)
-        h_prev_after = self.hprev
         if self.CHECK_GRADS:
-            self.hprev = hprev
-            num_grads = self.ComputeGradsNum(X,Y,1e-4)
-            self.hprev = h_prev_after
+            # self.hprev = hprev
+            hprev = self.hprev
+            num_grads = self.ComputeGradsNum(X,Y,hprev,1e-4)
             D = {}
             S = {}
             RD = {}
             for f in self.fieldnames:
                 D[f] = grads[f] - num_grads[f]
+                if f == 'W':
+                    print (D[f][0],'\n', grads[f][0], '\n', num_grads[f][0])
                 S[f] = np.abs(grads[f]) + np.abs(num_grads[f])
                 RD[f] = np.mean(np.abs(D[f])) / np.mean(S[f])
-                print ('Relative difference in %s is ' % (f), RD[f])
+                print ('Relative difference in %s is' % (f), RD[f])
         # update
-        for f in self.fieldnames:
-            self.grad[f] -= self.eta * grads[f]
-        return
+        # for f in self.fieldnames:
+        #     self.para[f] -= self.eta * grads[f]
+        return grads
 
     def train_network(self):
+        ite = 0
+        list_loss = []
         smooth_loss = 0
+
+        min_L = 10000000
+        best_para = self.init_para()
+
         for i in range(MAX_EPOCH):
             e = 0
-            loss = 0
-            ite = 0
-            while(e+self.seq_length <= len(self.data)):
+            m = self.init_para()
+            while(e+self.seq_length <= len(self.data)-1):
                 X, Y = self.get_data(e)
                 L, H, A, P = self.forward_pass(self.para, self.hprev, X, Y)
-                loss = L
                 # print ('loss in epoch %d' % i)
-                self.backward_pass(X, Y, H, A, P)
+                grads = self.backward_pass(X, Y, H, A, P)
+                if self.CLIP:
+                    grads = self.grad_clip(grads)
+                # AdaGrad
+                if self.ADA:
+                    for f in self.fieldnames:
+                        m[f] += np.square(grads[f])
+                        self.para[f] -= (self.eta / np.sqrt(m[f] + 1e-3)) * grads[f]
+                else:
+                    for f in self.fieldnames:
+                        self.para[f] -= self.eta * grads[f]
 
-                smooth_loss = 0.999 * smooth_loss + 0.001 * loss
-                if (e % 10000 == 0):
-                    print ('e=', e)
-                    print ('Smooth loss in iter %d is %.2f' % (i+1, smooth_loss))
-                    x0 = ind_to_vec(int(np.round(np.random.random()*total_num_char)))
-                    self.generate_texts(200, self.h0, x0)
+                smooth_loss = 0.999 * smooth_loss + 0.001 * L
+                list_loss.append(smooth_loss)
 
-                e += seq_length
+                if (L < min_L):
+                    best_para = copy.deepcopy(self.para)
+                    min_L = L
+
+
+                if ((ite+1) % 10000 == 0):
+                    print ('\nSmooth loss in epoch %d, iter %d is %.2f\n' % (i+1, ite+1, smooth_loss))
+                    x0 = ind_to_vec(int(np.round(np.random.random()*total_num_char-1)))
+                    self.generate_texts(200, self.h0, x0, self.para)
+
+                e += self.seq_length
+                # print (e)
                 ite += 1
-            self.h_prev = self.h0
+                self.hprev = H[-1]
+            self.hprev = self.h0
 
 
-        print ('Training done for %d iterations\n' % (MAX_EPOCH))
-        x0 = ind_to_vec(int(np.round(np.random.random()*total_num_char)))
-        self.generate_texts(200, self.h0, x0)
+        print ('\nTraining done for %d iterations\n' % (MAX_EPOCH))
+        x0 = ind_to_vec(int(np.round(np.random.random()*(total_num_char-1))))
+        print ('Minimum loss is %f, generating texts of length 1000 with the relative parameters...\n' % min_L)
+        self.generate_texts(1000, self.h0, x0, best_para)
+        return list_loss
 
 if __name__ == '__main__':
     # Read the data into a consecutive sequence
@@ -302,9 +370,10 @@ if __name__ == '__main__':
 
     R = RNN(data, total_num_char, total_num_char, hid_m, eta, seq_length, [char_to_ind,ind_to_char])
     h0 = np.random.normal(0,1,(hid_m,1))
-    ind0 = int(np.round(np.random.random()*total_num_char))
+    ind0 = int(np.round(np.random.random()*total_num_char-1))
     x0 = ind_to_vec(ind0)
     # print ('Initial input character is %s\n' % (ind_to_char[ind0]))
     # texts = R.generate_texts(200,h0,x0)
     # print ('After initialization, generated text from random x0 is:\n\n%s' % (texts))
-    R.train_network()
+    l = R.train_network()
+    plot_loss(l)
